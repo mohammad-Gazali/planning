01837f41b5f364a7f4b48171836447facbcebeb7
Mohammed <m.alshahrani2012@gmail.com>
2022-06-29 07:17:56 +0300

    تحديث الخارطة

--- Schools/urls.py
+++ Schools/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('details/<str:s>',views.details,name='detail'),
     path('offices_list',views.offices_list,name='offices_list'),
     path('office_details/<str:office_name>',views.office_details,name='office_details'),
-    
+    path('projects/<str:project_type>',views.all_projects,name='all_projects'),
+    path('ProjectsTypes/',views.ProjectTypes,name='ProjectsTypes'),
+
     ]
